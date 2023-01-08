dd35267e9aadcce027e9edca195d3f22ccc164e7
Akash Roy <akashrou72@gmail.com>
2023-01-08 16:35:02 +0530

    menu fixed

--- js/main.js
+++ js/main.js
@@ -419,111 +419,97 @@ $(function(){
 });
 // table expandable js ends here
 
-// text editor js sdtarts here
-const options = {
-    modules: {
-        toolbar: [[{ font: [] }, { size: [] }], ['bold', 'italic', 'underline', 'strike'], [{ color: [] }, { background: [] }], [{ script: 'super' }, { script: 'sub' }], [{ header: '1' }, { header: '2' }, 'blockquote', 'code-block'], [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }], ['direction', { align: [] }], ['link', 'image', 'video', 'formula'], ['clean']],
-    },
-    theme: 'snow',
-};
-const editor = new Quill('#editor', options);
-// text editor js ends here
 
 // toggle js and left modal js starts here
 $(document).ready(function(){
     $("#left_modal_sm").modal('show');
-      $("#dark-mode").click(function(){
+    $("#dark-mode").click(function(){
           $("body").addClass("darkmodeactivated");
           $("#dark-mode").css("display","none");
           $("#light-mode").css("display","block");
           $(".light-mood-logo").css("display","none");
           $(".dark-mood-logo").css("display","block");
-  
-      })
-  
-      $("#light-mode").click(function(){
-          $("body").removeClass("darkmodeactivated");
-          $("#dark-mode").css("display","block");
+          
+        })
+        
+        $("#light-mode").click(function(){
+            $("body").removeClass("darkmodeactivated");
+            $("#dark-mode").css("display","block");
           $("#light-mode").css("display","none");
           $(".light-mood-logo").css("display","block");
           $(".dark-mood-logo").css("display","none");
-      })
-  
-      $("#collapse_btn").click(function(){
-        $(".left-card-wrap").toggleClass("shrink");
-        $(".right-card-wrap").toggleClass("expand");
+        })
+        
+        $("#collapse_btn").click(function(){
+            $(".left-card-wrap").toggleClass("shrink");
+            $(".right-card-wrap").toggleClass("expand");
         $(".ver-tab-txt").toggleClass("ver-tab-txt-d-none");
         $(".fa-angle-left").toggleClass("rotate");
-      })
+    })
   
       $("#srch_btn").click(function(){
         $(".fixed-search-panel").toggleClass("active-search-panel");
         
         $("#left_modal_sm").modal('show');
-      })
-  
-      $(".add_btn").click(function(){      
+    })
+    
+    $(".add_btn").click(function(){      
         $("#right_modal_xl").modal('show');
-      })
-  
-      $("#srch-btn").click(function(){
-        $(".fixed-search-panel").removeClass("active-search-panel");
-        $(".nav-transparent-bg").removeClass("nav-transparent-bg-active");
-        $(".fixed-search-panel").removeClass("animate__animated");
-        $("#srch_btn").css("display","block");
-        $(".show-hide-panel").addClass("panel-active");
-        $("#left_modal_sm").modal('hide');
-      })
-      $("#create-btn").click(function(){
-        $(".fixed-search-panel").removeClass("active-search-panel");
-        $(".nav-transparent-bg").removeClass("nav-transparent-bg-active");
-        $(".fixed-search-panel").removeClass("animate__animated");
-        $("#srch_btn").css("display","block");
-        $(".show-hide-panel").addClass("panel-active");
-        $("#left_modal_sm").modal('hide');
-      })
-      $("#create-btn-2").click(function(){
-        $(".fixed-search-panel").removeClass("active-search-panel");
-        $(".nav-transparent-bg").removeClass("nav-transparent-bg-active");
-        $(".fixed-search-panel").removeClass("animate__animated");
-        $("#srch_btn").css("display","block");
-        $(".show-hide-panel").addClass("panel-active");
-        $("#left_modal_sm").modal('hide');
-      })
-  
-      $(".show-single-panel-btn").click(function(){
-        $(".tab-panel").slideToggle("slow");
-        $(".show-single-panel-btn").toggleClass("rotate");
-      })
     })
   
+    $("#srch-btn").click(function(){
+        $(".fixed-search-panel").removeClass("active-search-panel");
+        $(".nav-transparent-bg").removeClass("nav-transparent-bg-active");
+        $(".fixed-search-panel").removeClass("animate__animated");
+        $("#srch_btn").css("display","block");
+        $(".show-hide-panel").addClass("panel-active");
+        $("#left_modal_sm").modal('hide');
+    })
+    $("#create-btn").click(function(){
+        $(".fixed-search-panel").removeClass("active-search-panel");
+        $(".nav-transparent-bg").removeClass("nav-transparent-bg-active");
+        $(".fixed-search-panel").removeClass("animate__animated");
+        $("#srch_btn").css("display","block");
+        $(".show-hide-panel").addClass("panel-active");
+        $("#left_modal_sm").modal('hide');
+    })
+    $("#create-btn-2").click(function(){
+        $(".fixed-search-panel").removeClass("active-search-panel");
+        $(".nav-transparent-bg").removeClass("nav-transparent-bg-active");
+        $(".fixed-search-panel").removeClass("animate__animated");
+        $("#srch_btn").css("display","block");
+        $(".show-hide-panel").addClass("panel-active");
+        $("#left_modal_sm").modal('hide');
+    })
     
-    function profileFunction() {
-        document.getElementById("profile-dropdown").classList.toggle("show");
-      }
-  
-      function notificationFunction() {
-        document.getElementById("notification-dropdown").classList.toggle("noti-show");
-      }
-  
-      
-      
-      // window.onclick = function(event) {
-      //   console.log(event.currentTarget);
-      //   if (!event.target.matches('.dropbtn')) {
-      //     var dropdowns = document.getElementsByClassName("dropdown-content");
-      //     var i;
-      //     for (i = 0; i < dropdowns.length; i++) {
-      //       var openDropdown = dropdowns[i];
-      //       if (openDropdown.classList.contains('show')) {
-      //         openDropdown.classList.remove('show');
-      //       }
-      //     }
-      //   }
-      // }
-// toggle js and left modal js ends here
+    $(".show-single-panel-btn").click(function(){
+        $(".tab-panel").slideToggle("slow");
+        $(".show-single-panel-btn").toggleClass("rotate");
+    })
+})
 
 
-  
-  
-  
+function profileFunction() {
+    document.getElementById("profile-dropdown").classList.toggle("show");
+}
+
+function notificationFunction() {
+    document.getElementById("notification-dropdown").classList.toggle("noti-show");
+}
+
+
+
+// window.onclick = function(event) {
+    //   console.log(event.currentTarget);
+    //   if (!event.target.matches('.dropbtn')) {
+        //     var dropdowns = document.getElementsByClassName("dropdown-content");
+        //     var i;
+        //     for (i = 0; i < dropdowns.length; i++) {
+            //       var openDropdown = dropdowns[i];
+            //       if (openDropdown.classList.contains('show')) {
+                //         openDropdown.classList.remove('show');
+                //       }
+                //     }
+                //   }
+                // }
+                // toggle js and left modal js ends here
